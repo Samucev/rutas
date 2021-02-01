3f654a76c37a462df9d3dbec9dfb597a4fd63765
ADRIAN SUMAGUI SALAZAR TSAPP2MA2021 <adrian_sumagui_apps1ma1920@cev.com>
2021-02-01 14:21:03 +0100

    Comienzo de implementación de navegación entre pantallas.

--- rutas/ViewController.swift
+++ rutas/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+                              
     }
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+
+    @IBAction func loginButton(_ sender: Any) {
+        let vc = storyboard?.instantiateViewController(identifier: "routesVC") as! routeViewController
+        present(vc, animated: true)
+    }
+    
+    
+    /*func goToRoutes(){
+        
+        let vc = storyboard?.instantiateViewController(identifier: "routesVC") as! routeViewController
+        present(vc, animated: true)
+        
+    }*/
 
 
 }
